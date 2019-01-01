b99441b721bb6b2cead52e3974383f69b33579ac
davidAlfonsoHermelo <david.afonso.hermelo@gmail.com>
2019-01-01 15:41:21 -0500

    add emojidictmaker

---
+++ emojiDictMaker.py
@@ -0,0 +1,184 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+import utilsOs, time
+from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import NoSuchElementException
+
+
+def getEmojiAndName(browserElement, lowerCase=False):
+	'''
+	divide the element into emoji and label
+	'''
+	#get the emoji and prefered label
+	titleList = (browserElement.text).split(u' ')
+	#get the emoji
+	emoji = titleList[0]
+	#get the prefered label
+	label = u' '.join(titleList[1:])
+	#lower case if solicited
+	if lowerCase != False:
+		label = label.lower()
+	return emoji, label
+
+
+def emojiDictMaker(dumpTheDict=False):
+	'''
+	makes and dumps a dict of emojis with their name and alternative names
+	'''
+	emojiCharDict = {}
+	emojiNameDict = {}
+	emptyList = []
+	#urls from emojipedia
+	categoriesList = [u'https://emojipedia.org/people/', 
+					u'https://emojipedia.org/nature/', 
+					u'https://emojipedia.org/food-drink/', 
+					u'https://emojipedia.org/activity/', 
+					u'https://emojipedia.org/travel-places/', 
+					u'https://emojipedia.org/objects/', 
+					u'https://emojipedia.org/symbols/', 
+					u'https://emojipedia.org/flags/']
+	#browse all emojis category by category
+	for categoryUrl in tqdm(categoriesList):
+		#open the browser window
+		driver = webdriver.Firefox()
+		#get to the category url
+		driver.get(categoryUrl)
+		#get lists of emojis in each category
+		emojiListSection = driver.find_element_by_class_name(u'emoji-list')
+		emojiItems = emojiListSection.find_elements_by_tag_name(u'a')
+		emojiUrlList = []
+		#browse the list to get each individual emoji url
+		for emojiItem in emojiItems:
+			#get the url of the emoji page
+			emojiUrl = emojiItem.get_attribute(u'href') #emojiUrl = u'{0}{0}'.format(categoryUrl, emoji.get_attribute(u'href')[1:] )
+			emojiUrlList.append(emojiUrl)
+		#close the browser
+		driver.close()
+		#go to each saved url page
+		for emojiUrl in emojiUrlList:
+			#open the browser window
+			driver = webdriver.Firefox()
+			#go to the emoji url page
+			driver.get(emojiUrl)
+			#get the emoji and prefered label
+			emoji, prefLabel = getEmojiAndName(driver.find_element_by_xpath(u'//h1'), lowerCase=True)
+			#get the hashtags
+			hashtag = u'#{0}'.format(prefLabel.replace(u' ', u'').replace(u':', u''))
+			hashtagWithEmoji = u'#{0}{1}'.format(prefLabel.replace(u' ', u'').replace(u':', u''), emoji)
+			#save to the dicts
+			emojiCharDict[emoji] = {u'preferedLabel': prefLabel, u'alternativeLabel':[], u'relatedEmojis':[], u'category':( categoryUrl.split(u'/') )[-2], u'hashtags':[hashtag, hashtagWithEmoji]}
+			emojiNameDict[prefLabel] = emoji
+			#get the alternative labels #############################################
+			try:
+				altLabelsSection = driver.find_element_by_class_name(u'aliases')
+				altList = altLabelsSection.find_elements_by_tag_name(u'li')
+				for altElement in altList:
+					emojiAlt, altLabel = getEmojiAndName(altElement, lowerCase=True)
+					#save to the dicts
+					emojiCharDict[emoji][u'alternativeLabel'] = list(set(emojiCharDict[emoji].get(u'alternativeLabel')+[altLabel]))
+					emojiNameDict[altLabel] = emoji
+					#save the hashtags to the dict
+					hashtag = u'#{0}'.format(altLabel.replace(u' ', u'').replace(u':', u''))
+					hashtagWithEmoji = u'#{0}{1}'.format(altLabel.replace(u' ', u'').replace(u':', u''), emoji)
+					emojiCharDict[emoji][u'hashtags'] = list(set( emojiCharDict[emoji].get(u'hashtags')+[hashtag, hashtagWithEmoji] ))
+			except NoSuchElementException: pass
+			#if there is a colon ':' we add a couple of alternative labels
+			if u':' in prefLabel:
+				columnSplitLabel = prefLabel.split(u': ')
+				#we take only what's after the colon
+				afterColonLabel = columnSplitLabel[1]
+				#we get rid of the colon and inverse the order of the separated elements
+				inversedLabel = u'{0} {1}'.format(columnSplitLabel[1], columnSplitLabel[0])
+				#save to the dicts
+				emojiCharDict[emoji][u'alternativeLabel'] = list(set(emojiCharDict[emoji].get(u'alternativeLabel')+[afterColonLabel, inversedLabel]))
+				emojiNameDict[afterColonLabel] = emoji
+				emojiNameDict[inversedLabel] = emoji
+				#save the hashtags to the dict
+				hashtag1 = u'#{0}'.format(afterColonLabel.replace(u' ', u'').replace(u':', u''))
+				hashtagWithEmoji1 = u'#{0}{1}'.format(afterColonLabel.replace(u' ', u'').replace(u':', u''), emoji)
+				hashtag2 = u'#{0}'.format(inversedLabel.replace(u' ', u'').replace(u':', u''))
+				hashtagWithEmoji2 = u'#{0}{1}'.format(inversedLabel.replace(u' ', u'').replace(u':', u''), emoji)
+				emojiCharDict[emoji][u'hashtags'] = list(set( emojiCharDict[emoji].get(u'hashtags')+[hashtag1, hashtagWithEmoji1, hashtag2, hashtagWithEmoji2] ))
+			#get the apple name as an alternative label #############################
+			try:
+				appleLabelSection = driver.find_element_by_class_name(u'applenames')
+				appleList = appleLabelSection.find_elements_by_tag_name(u'p')
+				for appleElement in appleList:
+					emojiAlt, altLabel = getEmojiAndName(appleElement, lowerCase=True)
+					#save to the dicts
+					emojiCharDict[emoji][u'alternativeLabel'] = list(set(emojiCharDict[emoji].get(u'alternativeLabel')+[altLabel]))
+					emojiNameDict[altLabel] = emoji
+					#save the hashtags to the dict
+					hashtag = u'#{0}'.format(altLabel.replace(u' ', u'').replace(u':', u''))
+					hashtagWithEmoji = u'#{0}{1}'.format(altLabel.replace(u' ', u'').replace(u':', u''), emoji)
+					emojiCharDict[emoji][u'hashtags'] = list(set( emojiCharDict[emoji].get(u'hashtags')+[hashtag, hashtagWithEmoji] ))
+			except NoSuchElementException: pass
+			#get the unicode name as an alternative label ###########################
+			try:
+				unicodeLabelSection = driver.find_element_by_class_name(u'unicodename')
+				unicodeList = unicodeLabelSection.find_elements_by_tag_name(u'p')
+				for unicodeElement in unicodeList:
+					emojiAlt, altLabel = getEmojiAndName(unicodeElement, lowerCase=True)
+					#save to the dicts
+					emojiCharDict[emoji][u'alternativeLabel'] = list(set(emojiCharDict[emoji].get(u'alternativeLabel')+[altLabel]))
+					emojiNameDict[altLabel] = emoji
+					#save the hashtags to the dict
+					hashtag = u'#{0}'.format(altLabel.replace(u' ', u'').replace(u':', u''))
+					hashtagWithEmoji = u'#{0}{1}'.format(altLabel.replace(u' ', u'').replace(u':', u''), emoji)
+					emojiCharDict[emoji][u'hashtags'] = list(set( emojiCharDict[emoji].get(u'hashtags')+[hashtag, hashtagWithEmoji] ))
+			except NoSuchElementException: pass
+			#get the shortCodes name as an alternative label #########################
+			try:
+				shortcodeLabelSection = driver.find_element_by_class_name(u'shortcodes')
+				shortcodeList = shortcodeLabelSection.find_elements_by_tag_name(u'li')
+				for shortcodeElement in shortcodeList:
+					shortCode = (shortcodeElement.text).lower().replace(u':', u'').replace(u'_', u' ')
+					#save to the dicts
+					emojiCharDict[emoji][u'alternativeLabel'] = list(set(emojiCharDict[emoji].get(u'alternativeLabel')+[shortCode]))
+					emojiNameDict[shortCode] = emoji
+					#save the hashtags to the dict
+					hashtag = u'#{0}'.format(shortCode.replace(u' ', u'').replace(u':', u''))
+					hashtagWithEmoji = u'#{0}{1}'.format(shortCode.replace(u' ', u'').replace(u':', u''), emoji)
+					emojiCharDict[emoji][u'hashtags'] = list(set( emojiCharDict[emoji].get(u'hashtags')+[hashtag, hashtagWithEmoji] ))
+			except NoSuchElementException: pass
+			#get the related emojis ##################################################
+			try:
+				relatedLabelSection = driver.find_element_by_xpath(u'/html/body/div[3]/div[1]/article/ul[3]')
+				relatedList = relatedLabelSection.find_elements_by_tag_name(u'a')
+				for relatedElement in relatedList:
+					relatedEmoji, relatedName = getEmojiAndName(relatedElement, lowerCase=True)
+					#save to the dict
+					emojiCharDict[emoji].get(u'relatedEmojis').append( [relatedEmoji, relatedName] )
+			except NoSuchElementException: pass
+			#close the browser
+			driver.close()
+	#dump the dicts to json files
+	utilsOs.dumpDictToJsonFile(emojiCharDict, pathOutputFile=u'./emojiDict/emojiCharDict.json', overwrite=True)
+	utilsOs.dumpDictToJsonFile(emojiNameDict, pathOutputFile=u'./emojiDict/emojiNameDict.json', overwrite=True)
+
+
+def augmentEmojiDict(dumpTheDict=False):
+	'''	'''
+	emojiCharDictPath = u'./emojiDict/emojiCharDict.json'
+	emojiNameDictPath = u'./emojiDict/emojiNameDict.json'
+	emojiCharDict = utilsOs. openJsonFileAsDict(emojiCharDictPath)
+	emojiNameDict = utilsOs. openJsonFileAsDict(emojiNameDictPath)
+	#browse the dict to augment
+	for emojiKey, emojiDict in emojiCharDict.itens():
+		#augment by splitting on the labels
+		if u':' in emojiDict[u'preferedLabel']:
+
+		#add hashtags
+	#dump the dicts to json files
+	utilsOs.dumpDictToJsonFile(emojiCharDict, pathOutputFile=u'./emojiDict/emojiCharDictExt.json', overwrite=True)
+	utilsOs.dumpDictToJsonFile(emojiNameDict, pathOutputFile=u'./emojiDict/emojiNameDictExt.json', overwrite=True)
+
+
+#emojiDictMaker(dumpTheDict=True)
+augmentEmojiDict(dumpTheDict=True)
--- simpleInstaBot.py
+++ simpleInstaBot.py
@@ -2,12 +2,14 @@
 #-*- coding:utf-8 -*-
 
 
-import time, random
+import time, random, cv2, urllib.request
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 ####################################################################################
 #SELENIUM GENERAL FUNCTIONS
@@ -17,6 +19,7 @@ def waitForPageToLoad(driver, xpathToElem, maxWaitTimeSeconds=25):
 	''' waits for the page to load and returns the element '''
 	element = WebDriverWait(driver, maxWaitTimeSeconds).until(EC.presence_of_element_located((By.XPATH, xpathToElem)))
 	return element
+
 
 def scrollPageWithInfiniteLoading(driver, limit=float('inf')):
 	'''
@@ -40,25 +43,54 @@ def scrollPageWithInfiniteLoading(driver, limit=float('inf')):
 		counter += 1
 
 
+def loadImageFromUrl(imageUrl, showImage=False):
+	'''
+	given an url it load the image ready to be used by openCV
+	based on https://prateekvjoshi.com/2016/03/01/how-to-read-an-image-from-a-url-in-opencv-python/
+	'''
+	#get the url response
+	urlResponse = urllib.request.urlopen(imageUrl)
+	#convert it to an array
+	imgArray = np.array(bytearray(urlResponse.read()), dtype=np.uint8)
+	#decode the image
+	img = cv2.imdecode(imgArray, -1)
+	#show the image
+	if showImage != False:
+		cv2.imshow('URL Image', img)
+		cv2.waitKey()
+	return img
+
+
+def detectFaces(imageUrl, showImage=False):
+	'''
+	uses opencv to recognize faces
+	based on https://realpython.com/face-recognition-with-python/
+	'''
+	#Create the haar cascade
+	faceCascade = cv2.CascadeClassifier('./faceDetection/haarcascade_frontalface_default.xml')
+	#Read the image
+	image = loadImageFromUrl(imageUrl)
+	#transform the image to greyscale so openCv can work on it
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	#Detect faces in the image by loading the face cascade to memory and processing the image
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(30, 30),
+		flags = cv2.CASCADE_SCALE_IMAGE) #faces is a list of rectangles in which it believes it found a face, each rectangle is 4 values: the x and y location of the rectangle, and the rectangle’s width and height (w , h)
+	#show the image
+	if showImage != False:
+		for (x, y, w, h) in faces:
+			cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		cv2.imshow('Image with found faces', image)
+		cv2.waitKey(0)
+	return faces
+
+
 ####################################################################################
 #INSTAGRAM SPECIFIC FUNCTIONS
 ####################################################################################
-
-def logInInstagram(driver, instagramUsername, instagramPassword):
-	'''
-	opens the login page of instagram and logs in
-	'''
-	driver.get('https://www.instagram.com/accounts/login/')
-	time.sleep(random.uniform(0.8, 3.1))
-	#username
-	userName = driver.find_element_by_xpath('//article/div/div/div/form/div/div/div/input')
-	userName.send_keys(instagramUsername)
-	#password
-	password = driver.find_element_by_xpath('//article/div/div/div/form/div[2]/div/div/input') 
-	password.send_keys(instagramPassword)
-	#find element
-	driver.find_element_by_xpath('//article/div/div/div/form/div[3]/button').click()
-
 
 def posponeNotifications(driver):
 	'''
@@ -70,6 +102,22 @@ def posponeNotifications(driver):
 		driver.find_element_by_xpath('//body/div[3]/div/div/div/div[3]/button[2]').click()
 	except Exception:
 		pass
+
+
+def instagramSearch(hashtagWord):
+	'''
+	make a search in the instagram search page
+	'''
+	#get to the search page
+	driver.get('https://www.instagram.com/explore/')
+	#handle the search bar
+	searchBar = driver.find_element_by_xpath('//nav/div[2]/div/div/div[2]/input')
+	searchBar.send_keys(u'#{0}'.format(hashtagWord))
+	time.sleep(random.uniform(0.4, 0.8))
+	searchBar.send_keys(Keys.RETURN)
+	searchBar.send_keys(Keys.RETURN)
+	time.sleep(random.uniform(1.9, 2.4))
+	driver.refresh()
 
 
 def clickHeartIfEmpty(driver, xpathToHeart, totalLikes=0):
@@ -89,6 +137,39 @@ def clickHeartIfEmpty(driver, xpathToHeart, totalLikes=0):
 	except Exception:
 		pass
 	return totalLikes
+
+
+def getLikeScore(driver, xpathToLikeScore):
+	'''
+	given the xpath to the like score element,
+	returns the likeScore as an integer
+	'''
+	likeScore = driver.find_element_by_xpath(xpathToLikeScore)
+	likeScore = (likeScore.text).replace(' likes', '')
+	likeScore = (likeScore).replace(' like', '')
+	if likeScore == 'like this':
+		likeScore = False 
+	return int(likeScore)
+
+
+####################################################################################
+#INSTABOT FUNCTIONS
+####################################################################################
+
+def logInInstagram(driver, instagramUsername, instagramPassword):
+	'''
+	opens the login page of instagram and logs in
+	'''
+	driver.get('https://www.instagram.com/accounts/login/')
+	time.sleep(random.uniform(0.8, 3.1))
+	#username
+	userName = driver.find_element_by_xpath('//article/div/div/div/form/div/div/div/input')
+	userName.send_keys(instagramUsername)
+	#password
+	password = driver.find_element_by_xpath('//article/div/div/div/form/div[2]/div/div/input') 
+	password.send_keys(instagramPassword)
+	#find element
+	driver.find_element_by_xpath('//article/div/div/div/form/div[3]/button').click()
 
 
 def likePicsYouFollow(driver, likeLimit=random.randint(7, 41), goToHomePage=False):
@@ -114,35 +195,14 @@ def likePicsYouFollow(driver, likeLimit=random.randint(7, 41), goToHomePage=Fals
 		time.sleep(random.uniform(0.8, 1.3))
 
 
-def getLikeScore(driver, xpathToLikeScore):
-	'''
-	given the xpath to the like score element,
-	returns the likeScore as an integer
-	'''
-	likeScore = driver.find_element_by_xpath(xpathToLikeScore)
-	likeScore = (likeScore.text).replace(' likes', '')
-	likeScore = (likeScore).replace(' like', '')
-	if likeScore == 'like this':
-		likeScore = False 
-	return int(likeScore)
-
-
 def likeRandomPics(driver, likeLimit=random.randint(850, 1045), hashtagWord=None, maxLikeScore=None):
 	'''
 	makes a search and starts liking pics with the searched tag
 	'''
 	if hashtagWord == None:
 		hashtagWord = u'flower'
-	#get to the search page
-	driver.get('https://www.instagram.com/explore/')
-	#handle the search bar
-	searchBar = driver.find_element_by_xpath('//nav/div[2]/div/div/div[2]/input')
-	searchBar.send_keys(u'#{0}'.format(hashtagWord))
-	time.sleep(random.uniform(0.4, 0.8))
-	searchBar.send_keys(Keys.RETURN)
-	searchBar.send_keys(Keys.RETURN)
-	time.sleep(random.uniform(1.9, 2.4))
-	driver.refresh()	
+	#make a search in instagram
+	instagramSearch(hashtagWord)	
 	#select all first appearing pictures
 	picturesLink = driver.find_elements_by_class_name('eLAPa')
 	#click on the first picture
@@ -173,8 +233,43 @@ def likeRandomPics(driver, likeLimit=random.randint(850, 1045), hashtagWord=None
 		time.sleep(random.uniform(0.8, 2.8))
 
 
-def likeAndComment():
-	''''''
+def likeAndCommentRandomPics(driver, likeLimit=random.randint(850, 1045), hashtagWord=None):
+	'''
+	makes a search and starts liking pics with the searched tag
+	'''
+	if hashtagWord == None:
+		hashtagWord = u'flower'
+	#make a search in instagram
+	instagramSearch(hashtagWord)	
+	#select all first appearing pictures
+	#picturesLink = driver.find_elements_by_class_name('eLAPa')
+	#click on the first picture
+	action = webdriver.common.action_chains.ActionChains(driver)
+	action.move_to_element_with_offset(driver.find_element_by_class_name('eLAPa'), 5, 6) #move cursor to 5 pixels down the top left corner and 6 pixels to the right of the top left corner
+	action.click()
+	action.perform()
+	#click pass the most popular pictures to the most recent pictures
+	nextPic = driver.find_element_by_xpath('//body/div[3]/div/div[1]/div/div/a')#click past the first popular picture
+	nextPic.click()
+	time.sleep(random.uniform(0.8, 1.5))
+	#the xpath of the next button changes after the first picture
+	for i in range(8):
+		nextPic = driver.find_element_by_xpath('//body/div[3]/div/div[1]/div/div/a[2]')
+		nextPic.click()
+		time.sleep(random.uniform(0.8, 1.5))
+	#start liking and commenting
+
+	#get image url and image content description
+	imageElement = driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/article/div[1]/div/div/div[1]/img')
+	imageUrl = imageElement.get_attribute('src')
+	imageContent = ( imageElement.get_attribute('alt').replace('Image may contain: ', '') ).split(', ')
+	print(imageContent)
+	
+
+
+####################################################################################
+#COMMANDS
+####################################################################################
 
 #open the browser window
 driver = webdriver.Firefox()
@@ -185,7 +280,8 @@ logInInstagram(driver, instagramUsername, instagramPassword)
 posponeNotifications(driver)
 time.sleep(1.0)
 ####likePicsYouFollow(driver)
-likeRandomPics(driver, likeLimit=5, hashtagWord=None)
+####likeRandomPics(driver, likeLimit=5, hashtagWord=None)
+likeAndCommentRandomPics(driver, likeLimit=5, hashtagWord=None)
 
 #close the browser window
 ###driver.close()
